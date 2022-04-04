a6fe9f95872eabe17186bdd0d0e5bbd529773bae
David Echoda <Davidechoda123@gmail.com>
2022-04-05 00:50:35 +0100

    code refactoring

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
-export default getNeighborhoodsList = () => {
-  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
-};
+}
