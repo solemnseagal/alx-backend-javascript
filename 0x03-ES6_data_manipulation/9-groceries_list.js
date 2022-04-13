441279d8c30bb78400ec8c5a8e7c8416de127a19
David Echoda <davidechoda123@gmail.com>
2022-04-13 02:50:40 +0000

    rest of tasks

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+/**
+ * Retrieves a map of grocery names and their quantity.
+ * @returns {Map<string, Number>}
+ */
+export default function groceriesList() {
+  const items = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(items);
+}
