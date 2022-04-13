e5b06b06b9a72c2b291d58462b57ca8a75c2a0d2
David Echoda <davidechoda123@gmail.com>
2022-04-13 02:38:19 +0000

    return array of ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
+export default function getListStudentIds(stds) {
+  if (stds instanceof Array) {
+    return stds.map((std) => std.id);
+  }
+  return [];
+}
